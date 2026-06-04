1af50ed29d6d1f708d94e294a68275bd899b607c
piggycatfofo-lab <piggycatfofo@gmail.com>
2026-06-03 22:28:01 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made By Piggycatfofo",
+        name: "Made For Illinois State Roleplay.",
         // Activity type number (0 = Playing).
         type: 0, 
       },
